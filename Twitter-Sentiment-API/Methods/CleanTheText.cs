8a8c7a6a25633ceca872e8a1b68baba2b68f4266
vahidkianfar <vahidkianfar@gmail.com>
2022-07-07 18:52:18 +0100

    Moved StopWords into Another File

---
+++ Twitter-Sentiment-API/Methods/CleanTheText.cs
@@ -0,0 +1,30 @@
+﻿namespace Twitter_Sentiment_API.Methods;
+
+public static class CleanTheText
+{
+    public static List<string> StopWordsFilter()
+    {
+        var stopWords = new List<string>
+            { "i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you", "your",
+                "yours", "yourself","we’ll","it’s",
+                "yourselves", "he", "him", "his", "himself", "she", "her", "hers", "herself", "it", "its", "itself",
+                "they", "them", "their", "theirs", "themselves", "what", "which", "who", "whom", "this", "that",
+                "these","doesn't", "all","it's",
+                "those", "am", "is", "are", "was", "were", "be", "been", "being", "have", "has", "had", "having", "do",
+                "does", "did", "doing", "a", "an", "the", "and", "but", "if", "or", "because", "as", "until", "while",
+                "of", "at", "by", "for", "with", "about", "against", "between", "into", "through", "during", "before",
+                "after", "above", "below", "to", "from", "up", "down", "in", "out", "on", "off", "over", "under",
+                "again","join", "all", "any", "both", "each", "few", "more", "most", "other", "some", "such", "no", "nor","us",
+                "further", "then", "once", "here", "there", "when", "where", "why", "how", "all", "any", "both", "each",
+                "few", "more", "most", "other", "some", "such", "no", "nor", "not", "only", "own", "same", "so", "than",
+                "too", "very", "s", "t", "can", "will", "just", "don", "should", "now", "'", "nh", "we'll", "didn't", "they'll",
+                "the","haven't", "you've", "they've", "we've", "i've", "you'll", "they'll", "we'll", "i'll", "you're",
+                "they're", "we're", "i'm", "you've", "they've", "we've","if", "https", "http", "get", "please","come", "here", "there", "when", "where", "why", "how", "all", "any", "both", "each", "few", "more", "most", "other", "some", "such", "no", "nor", "not", "only", "own", "same", "so", "than", "too", "very", "s", "t", "can", "will", "just", "don", "should", "now", "'", "nh", "we'll", "didn't", "they'll", "the", "haven't", "you've", "they've", "we've", "i've", "you'll", "they'll", "we'll", "i'll", "you're", "they're", "we're", "i'm", "you've", "they've", "we've", "if", "https", "http", "get", "please", "come", "here", "there", "when", "where", "why", "how", "all", "any", "both", "each", "few", "more", "most", "other", "some", "such", "no", "nor", "not", "only", "own", "same", "so", "than", "too", "very", "s", "t", "can", "will", "just", "don", "should", "now", "'", "nh", "we'll", "didn't", "they'll", "the", "haven't", "you've", "they've", "we've", "i've", "you'll", "they'll", "we'll", "i'll", "you're", "they're", "we're", "i'm", "you've", "they've", "we've", "if", "https", "http", "get", "please", "come", "here", "there", "when", "where", "why", "how", "all", "any", "both", "each", "few", "more", "most", "other", "some", "such", "no", "nor", "not", "only", "own", "same", "so", "than", "too", "very", "s", "t", "can",
+                "we'll", "didn't", "they'll",
+                "the","haven’t", "you’ve", "they’ve", "we’ve", "i’ve", "you’ll", "they’ll", "we’ll", "i’ll", "you’re",
+                "they’re", "we’re", "i’m", "you’ve", "they’ve", "we’ve", "doesn’t","https","t","co"
+            };
+
+        return stopWords;
+    }
+}
--- Twitter-Sentiment-API/Services/HttpServices.cs
+++ Twitter-Sentiment-API/Services/HttpServices.cs
@@ -5,6 +5,7 @@ using System.Text.Json;
 using System.Text.Json.Nodes;
 using System.Text.RegularExpressions;
 using DeepAI;
+using Twitter_Sentiment_API.Methods;
 using Twitter_Sentiment_API.Models;
 
 namespace Twitter_Sentiment_API.Services;
@@ -88,7 +89,7 @@ public class HttpServices
             Console.WriteLine(word);
         }
         List<string> stopWordsList = new List<string>();
-        stopWordsList = StopWordsFilter();
+        stopWordsList = CleanTheText.StopWordsFilter();
         var newWords = words.Where(word => !stopWordsList.Contains(word));
         text = string.Join(" ", newWords).ToLower();
         Console.WriteLine(text);
@@ -108,31 +109,7 @@ public class HttpServices
         // return response;
     }
 
-    public List<string> StopWordsFilter()
-    {
-        var stop_words = new List<string>
-            { "i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you", "your",
-                "yours", "yourself","we’ll","it’s",
-                "yourselves", "he", "him", "his", "himself", "she", "her", "hers", "herself", "it", "its", "itself",
-                "they", "them", "their", "theirs", "themselves", "what", "which", "who", "whom", "this", "that",
-                "these","doesn't", "all","it's",
-                "those", "am", "is", "are", "was", "were", "be", "been", "being", "have", "has", "had", "having", "do",
-                "does", "did", "doing", "a", "an", "the", "and", "but", "if", "or", "because", "as", "until", "while",
-                "of", "at", "by", "for", "with", "about", "against", "between", "into", "through", "during", "before",
-                "after", "above", "below", "to", "from", "up", "down", "in", "out", "on", "off", "over", "under",
-                "again","join", "all", "any", "both", "each", "few", "more", "most", "other", "some", "such", "no", "nor","us",
-                "further", "then", "once", "here", "there", "when", "where", "why", "how", "all", "any", "both", "each",
-                "few", "more", "most", "other", "some", "such", "no", "nor", "not", "only", "own", "same", "so", "than",
-                "too", "very", "s", "t", "can", "will", "just", "don", "should", "now", "'", "nh", "we'll", "didn't", "they'll",
-                "the","haven't", "you've", "they've", "we've", "i've", "you'll", "they'll", "we'll", "i'll", "you're",
-                "they're", "we're", "i'm", "you've", "they've", "we've","if", "https", "http", "get", "please","come", "here", "there", "when", "where", "why", "how", "all", "any", "both", "each", "few", "more", "most", "other", "some", "such", "no", "nor", "not", "only", "own", "same", "so", "than", "too", "very", "s", "t", "can", "will", "just", "don", "should", "now", "'", "nh", "we'll", "didn't", "they'll", "the", "haven't", "you've", "they've", "we've", "i've", "you'll", "they'll", "we'll", "i'll", "you're", "they're", "we're", "i'm", "you've", "they've", "we've", "if", "https", "http", "get", "please", "come", "here", "there", "when", "where", "why", "how", "all", "any", "both", "each", "few", "more", "most", "other", "some", "such", "no", "nor", "not", "only", "own", "same", "so", "than", "too", "very", "s", "t", "can", "will", "just", "don", "should", "now", "'", "nh", "we'll", "didn't", "they'll", "the", "haven't", "you've", "they've", "we've", "i've", "you'll", "they'll", "we'll", "i'll", "you're", "they're", "we're", "i'm", "you've", "they've", "we've", "if", "https", "http", "get", "please", "come", "here", "there", "when", "where", "why", "how", "all", "any", "both", "each", "few", "more", "most", "other", "some", "such", "no", "nor", "not", "only", "own", "same", "so", "than", "too", "very", "s", "t", "can",
-                "we'll", "didn't", "they'll",
-                "the","haven’t", "you’ve", "they’ve", "we’ve", "i’ve", "you’ll", "they’ll", "we’ll", "i’ll", "you’re",
-                "they’re", "we’re", "i’m", "you’ve", "they’ve", "we’ve", "doesn’t","https","t","co"
-            };
-
-        return stop_words;
-    }
+    
 
 
 }
